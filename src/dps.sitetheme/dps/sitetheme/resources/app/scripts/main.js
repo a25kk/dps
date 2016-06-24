7a3ccc98612184c59cd28bf76df552614da97c19
Serge Davidov <sd@kreativkombinat.de>
2016-06-24 12:38:32 +0200

    update contentpackage

--- src/dps.sitetheme/dps/sitetheme/resources/app/scripts/main.js
+++ src/dps.sitetheme/dps/sitetheme/resources/app/scripts/main.js
@@ -1,40 +1,15 @@
-'use strict';
+  'use strict';
 (function ($) {
   $(document).ready(function () {
-    var $bannerBar = document.querySelectorAll('.app-js-carousel'),
-        $galleryContainer = document.querySelectorAll('.js-gallery');
-    // Show banner bar
-    if ($bannerBar.length) {
-      var bannerflkty = new Flickity('.app-js-carousel', {
-        autoPlay: 7000,
-        contain: true,
-        wrapAround: true,
-        imagesLoaded: true,
-        cellSelector: '.app-banner-item',
-        cellAlign: 'left',
-        selectedAttraction: 0.025,
-        friction: 0.28
-      });
-    }
-    // Content image galleries
-    if ($galleryContainer.length) {
-      var flkty = new Flickity('.js-gallery', {
-        autoPlay: true,
-        contain: true,
-        wrapAround: true,
-        imagesLoaded: true,
-        cellSelector: '.app-gallery-cell',
-        cellAlign: 'left'
-      });
-    }
-
-    // Anonymous only scripts (mainly used in login views)
+    // Setup media query for enabling dynamic layouts only on
+    // larger screen sizes
+    var mq = window.matchMedia("(min-width: 320px)");
     if ($(".userrole-anonymous")[0]){
       $('input[type="password"]').showPassword('focus', {
       });
       $('.app-signin-input').jvFloat();
       var $mcNote = $('#app-signin-suggestion');
-      Mailcheck.defaultDomains.push('eda.kreativkombinat.de')
+      Mailcheck.defaultDomains.push('lcm.ade25.de')
       $('#ac-name').on('blur', function (event) {
         console.log("event ", event);
         console.log("this ", $(this));
@@ -58,11 +33,48 @@
               }
         });
       });
-      // Setup media query for enabling dynamic layouts only on
-      // larger screen sizes
-      var mq = window.matchMedia("(min-width: 480px)");
+      // Enable gallery and masonry scripts based on screen size
       if (mq.matches) {
-        // Enable e.g. masonry scripts based on available screen size
+        var bannerflkty = new Flickity('.app-banner', {
+          autoPlay: true,
+          contain: true,
+          wrapAround: true,
+          imagesLoaded: true,
+          cellSelector: '.app-banner-item',
+          cellAlign: 'left',
+          autoPlay: 7500
+        });
+        var flkty = new Flickity('.main-gallery', {
+          autoPlay: true,
+          contain: true,
+          wrapAround: true,
+          imagesLoaded: true,
+          cellSelector: '.app-gallery-cell',
+          cellAlign: 'left'
+        });
+        var $isoContainer = $('#js-isotope-container');
+        $isoContainer.addClass('js-isotope-intialized');
+        $isoContainer.isotope({
+          itemSelector: '.js-isotope-item',
+          layoutMode: 'fitRows',
+          animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+          },
+          resizable: false,
+          animationEngine: 'best-available'
+        });
+        // filter items on button click
+        var $selectionLinks = $('[data-appui="app-list-filter"]'),
+            $current = '';
+        $('#app-list-filters').on('click', '[data-appui="app-list-filter"]', function () {
+          $current = $(this);
+          $selectionLinks.not($current).removeClass('active-filter');
+          $(this).addClass('active-filter');
+          var $filterValue = $(this).attr('data-filter');
+          $isoContainer.isotope({ filter: $filterValue });
+        });
       }
     };
   }
